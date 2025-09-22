ae796f6a20385c311496361e2a143feac0dd64dd
jjanie <hmyeonge@gmail.com>
2025-09-22 18:47:55 +0900

    chore: Link, useNavigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Link, useNavigate } from 'react-router'
 import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import NotFound from './pages/NotFound'
 
 function App() {
+  const nav = useNavigate(); 
+
+  const handleButtonClick = () => {
+    nav('/new'); 
+  }
+
   return (
+    <>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/new">New</Link>
+      <Link to="/diary">Diary</Link>
+      <button onClick={handleButtonClick}>버튼</button>
+    </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary" element={<Diary />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+    </>
+
   )
 }
 export default App
